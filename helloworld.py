38d1fcac5eb028c5440bc0fdbaec81dc92418729
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-08-04 09:50:20 +0530

    Day 1

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+# String Literal using double quotes
+print("Hello, World")
+
+# String Literal using single quotes
+print('Hello, World')
+
+# String Literal using triplets
+print("""Hello, World""")
+print('''Hello, World''')
+
+'''
+Multi line
+comment using triplet
+'''
+print('''Hello, how are you?
+I'm fine. What's happening
+''')
